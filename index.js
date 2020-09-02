a9083bbdf5d87a97e130665252b5b641a4143402
Cyvision <cyvision13@outlook.com>
2020-09-02 16:24:26 +0430

    HTML Templating Filling The Templates

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const fs = require('fs');
+ const fs = require('fs');
 const http = require('http')
 const url = require('url');
 
+const replaceTemplate = (temp,product)=>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
+    output = output.replace(/{%IMAGE%}/g,product.image)
+    output = output.replace(/{%PRICE%}/g,product.price)
+    output = output.replace(/{%FROM%}/g,product.from)
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g,product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g,product.description)
+    output = output.replace(/{%ID%}/g,product.id)
+
+    if(!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
+        
+    }
+    return output
+    
+}
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const dataObj =  JSON.parse(data)
 
 http.createServer((req,res) =>{
     const pathName = req.url;
-    if(pathName === '/' ||  pathName === '/overview')
-        res.end('This is the OverView')
-    else if (pathName === '/product')
+    if(pathName === '/' ||  pathName === '/overview'){
+        res.writeHead(200,{
+            'Content-type':'text/html'            
+        });
+        const cardsHtml = dataObj.map(el=>{
+            return replaceTemplate(tempCard,el)
+        }).join('');
+       
+        const output = tempOverview.replace('{%PRODUCT_CART%}',cardsHtml)
+        res.end(output)
+    }
+        
+    else if (pathName === '/product'){
         res.end('This is the Product')
+    }
+        
     else if(pathName === '/api'){
         res.writeHead(200,{'Content-type':'application/json'})
         res.end(data)
